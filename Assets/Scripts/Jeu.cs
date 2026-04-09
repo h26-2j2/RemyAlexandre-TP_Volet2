4598c0f65e7005c0d6346bf241dd93c1b8cbf828
Reichowww <alexthamagicman@gmail.com>
2026-04-08 23:54:39 -0400

    Initial commit

---
+++ Assets/Scripts/Jeu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class Jeu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
