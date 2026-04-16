b1b70e2d05d8ac69b731c1065e4934bdaf62c32a
Reichowww <alexthamagicman@gmail.com>
2026-04-15 20:50:03 -0400

    debugging dans l'animator pour les transitions

--- Assets/Scripts/HoverBoutons.cs
+++ Assets/Scripts/HoverBoutons.cs
@@ -4,11 +4,17 @@ using UnityEngine.EventSystems;
 public class HoverBoutons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     // Ce que j'veux afficher au :hover et :active
-    public GameObject visuelHover; 
-    public GameObject visuelActive; 
+    public GameObject visuelHover;
+    public GameObject visuelActive;
+
+    Animator anim;
+    bool animHover = false;
+
 
     void Start()
     {
+        anim = GetComponentInChildren<Animator>();
+
         // Caché au lancement du jeu
         if (visuelHover != null)
         {
@@ -20,30 +26,42 @@ public class HoverBoutons : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
         }
     }
 
-    // Quand la souris entre sur le collider du parent
+    // Quand la souris HOVER sur le collider du parent
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (visuelHover != null)
         {
             visuelHover.SetActive(true);
         }
+
+        if (anim != null)
+        {
+            anim.SetBool("animHover", true);
+        }
     }
 
-    // Quand la souris sort du collider du parent
+    // Quand la souris SORT du collider du parent
     public void OnPointerExit(PointerEventData eventData)
     {
         if (visuelHover != null)
         {
             visuelHover.SetActive(false);
+
+            anim.SetBool("animHover", false);
+            anim.CrossFade("Désactivé", 0.05f); // Interruption immédiate, + fluide que la transition de retour dans l'animator
         }
+    }
+
+    public void Update()
+    {
+        // Debug.Log(anim.GetBool("animHover"));
     }
 
 
 
 
 
-
-    // Quand la souris clique dessus
+    // Quand la souris CLIQUE dessus
     public void visuelClicSouris() // Que j'active grâce au Event Trigger du parent
     {
         if (visuelActive != null)
